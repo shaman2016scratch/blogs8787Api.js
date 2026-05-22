455462684187ed2f1b51ddc4321a8b3deccf9523
shaman2016scratch <g.ilkh@yandex.ru>
2026-05-22 09:44:46 +0300

    [0.0.2] Add library info

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { accounts } from './src/components/accounts/main'
 import { blogs } from './src/components/blogs/main'
 import { info } from './src/components/info/main'
+import library from './src/lib/library'
 
 const blogs8787api = {
+    library,
     genAuth: async function() {
         const reqAuthCode = await fetch('https://api-shaman2016.vercel.app/blogs/genCode')
         const resAuthCode = await reqAuthCode.json()
---
+++ src/lib/library.js
@@ -0,0 +1,9 @@
+const library = {
+    name: 'Blogs8787api.js',
+    version: '0.0.2',
+    developers: [
+        { name: 'SHAMAN2016' }
+    ]
+}
+
+export default library
